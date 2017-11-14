036c66d575a6bf0895e08e63d01ce7d6d3b25c9c
caojiantao <13437104137@163.com>
2017-11-14 21:30:50 +0800

    登陆界面初始化

---
+++ src/view/login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="login-wrapper">
+    <img src="../assets/logo.png">
+    <el-form label-width="80px">
+      <el-form-item label="账号">
+        <el-input></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">提交</el-button>
+        <el-button>重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style>
+#login-wrapper{
+  width: 700px;
+  margin: 0 auto;
+}
+</style>
+
