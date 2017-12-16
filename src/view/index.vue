fd699a19016fa1ad224c8005deac677ed6c224be
caojiantao <13437104137@163.com>
2017-12-16 18:18:36 +0800

    首页dom结构修改

--- src/view/index.vue
+++ src/view/index.vue
@@ -1,12 +1,72 @@
 <template>
   <div>
-    <h2>这里是首页。</h2>
-    <router-link to="/login">登录</router-link>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">
+          <el-menu>
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="1-1">选项一</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <el-container>
+            <el-header>
+              <el-breadcrumb separator-class="el-icon-arrow-right">
+                <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+                <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+                <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+                <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+              </el-breadcrumb>
+            </el-header>
+            <el-main class="content">
+              main
+            </el-main>
+          </el-container>
+        </el-main>
+      </el-container>
+      <el-footer>Footer</el-footer>
+    </el-container>
   </div>
 </template>
 
+<style scoped>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .content {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+
+  .el-breadcrumb {
+    line-height: 60px;
+  }
+</style>
+
+
 <script>
-export default {
-  name: 'index'
-}
+  export default {
+    name: 'index'
+  }
 </script>
